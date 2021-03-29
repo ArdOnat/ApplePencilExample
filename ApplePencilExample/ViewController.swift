a55b5ab43f9e95e72e24bacfa2a02ff8089ab58b
Arda Onat <arda.onat@hepsiburada.com>
2021-03-29 18:09:05 +0300

    Fix navigation bar visibility.

--- ApplePencilExample/ViewController.swift
+++ ApplePencilExample/ViewController.swift
@@ -9,23 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         navigationController?.navigationBar.isHidden = true
     }
-    
-    @IBAction func pencilKitButtonPressed(_ sender: Any) {
-        
-    }
-    
-    @IBAction func scribbleButtonPressed(_ sender: Any) {
-        guard let drawingViewController = storyboard?.instantiateViewController(withIdentifier: "ScribbleViewController") as? ScribbleViewController,
-            let navigationController = navigationController else {
-                return
-        }
-        navigationController.pushViewController(drawingViewController, animated: true)
-    }
-    
 }
